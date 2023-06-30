7992c37885047278b1c0ca9b5c5c33de104505ce
LDY <leedy3838@naver.com>
2023-07-01 06:34:16 +0900

    Posts 도메인 개발중6 // 주석 처리

--- src/main/java/soongmyeong/hackathon/comments/CommentsResponseDto.java
+++ src/main/java/soongmyeong/hackathon/comments/CommentsResponseDto.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class CommentsResponseDto {
     //private Long id_comments;
     //private Long id_posts;
     //private Long id_user;
     private String content;
 
+    public CommentsResponseDto(String content){
+        this.content = content;
+    }
 }
